3944edae1c72506d4dd7448c077e505d2aa85acd
gabrioli <oliveiragabrielly.dev@gmail.com>
2026-06-10 16:55:00 -0300

    Simplificando

--- py_shadow_merge/py_shadow_merge.py
+++ py_shadow_merge/py_shadow_merge.py
@@ -1,18 +1,5 @@
 def shadow_merge(list1: list[int], list2: list[int]) -> list[int]:
-    merged_list = []
-    i = 0  
-    j = 0
-    while i < len(list1) and j < len(list2):
-        if list1[i] < list2[j]:
-            merged_list.append(list1[i])
-            i += 1
-        else:
-            merged_list.append(list2[j])
-            j += 1
-    merged_list.extend(list1[i:])
-    merged_list.extend(list2[j:])
-    
-    return merged_list
+    return sorted(list1 + list2)
 
 
 if __name__ == "__main__":
